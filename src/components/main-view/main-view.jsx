f499093ede63653240c79177a9d5f5ae26e346cd
herringg369 <herringg369@gmail.com>
2021-10-31 17:56:25 -0400

    recreating the myflixfrontend app

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import axios from 'axios'
+import { MovieCard } from '../movie-card/movie-card'
+import { MovieView } from '../movie-view/movie-view'
+
+export class MainView extends React.Component {
+
+    constructor(){
+        super()
+        //
+        this.state = {
+            movies: [],
+            selectedMovie: null, 
+        }
+    }
+
+    componentDidMount(){
+      axios.get('https://herringg369movieapi.herokuapp.com/movies').then(response => {
+        console.log(response.data)
+          this.setState({
+            movies: response.data}) }).catch(error => {
+          console.log(error);
+        });
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+          selectedMovie: newSelectedMovie
+        });
+      }
+
+      
+
+      render() {
+        const { movies, selectedMovie } = this.state;
+    
+        //if (movies.length === 0) return <div className="main-view" />;
+    
+        return (
+          <div className="main-view">
+          <h1>test</h1>
+            {selectedMovie
+              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+              : movies.map(movie => (
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+             ))
+            }
+          </div>
+        );
+      }
+    }
