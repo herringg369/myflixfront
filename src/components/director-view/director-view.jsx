93f95a8031199dcf989dd813df6fa57a168994f7
herringg369 <herringg369@gmail.com>
2021-11-20 15:18:38 -0500

    added director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class DirectorView extends React.Component {
   render() {
+    const { director } = this.props
+
    return (
-       <div>Directors</div>
+       <div className="director-view">
+          <h1>{director.Name}</h1>
+          <div className="directorBio">{director.Bio}</div>
+          <div className="directorBirth">{director.Birth}</div>
+          <div className="directorDeath">{director.Death}</div>
+          <div className="directorMovies">{director.Movies}</div>
+       </div>
    )
   }
 }
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string,
+    Birth : PropTypes.string,
+    Death: PropTypes.string,
+    Movies: PropTypes.array
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class GenreView extends React.Component {
   render() {
+    const { genre } = this.props
+
    return (
-       <div>Genres</div>
+       <div className="genre-view">
+        <h1></h1>
+
+       </div>
    )
   }
 }
+
+GenreView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string,
+    Birth : PropTypes.string,
+    Death: PropTypes.string,
+    Movies: PropTypes.array
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
